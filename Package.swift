3d4a4302d70a152bc3cda7ab1fb1a237869ff415
Dogfish <dyingsunlight@gmail.com>
2023-06-06 17:50:57 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,11 +1,4 @@
-// swift-tools-version:5.7
-/**
- This file is part of the Reductio package.
- (c) Sergio Fernández <fdz.sergio@gmail.com>
-
- For the full copyright and license information, please view the LICENSE
- file that was distributed with this source code.
- */
+// swift-tools-version:5.3
 
 import PackageDescription
 
